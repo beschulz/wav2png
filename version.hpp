185ba91ab37454256c127031b7bff7a2eeb878bc
Benjamin Schulz <beschulz@betabugs.de>
2012-06-15 15:00:29 +0200

    deleted version.hpp

--- version.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef VERSION_HPP__
-#define VERSION_HPP__
-namespace {
-	namespace version {
-		static const std::string date="Fri Jun 15 14:30:14 CEST 2012";
-		static const std::string version="0.3";
-	}; /* namespace version */
-} /* anonymous namespace */
-#endif /* VERSION_HPP__ */
